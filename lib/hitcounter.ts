ccc4145a4d890ffe0e5f98cd32387f6ecd9e832b
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2021-05-17 21:32:13 +0100

    Updated lambda.

---
+++ lib/hitcounter.ts
@@ -0,0 +1,35 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
+
+export interface HitCounterProps {
+    downstream: lambda.IFunction;
+
+}
+
+export class HitCounter extends cdk.Construct {
+
+    public readonly handler: lambda.Function; // allows access the counter function
+
+
+    constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+        super(scope, id);
+
+        const table = new dynamodb.Table(this, 'Hits', {
+            partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+        });
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_10_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        });
+
+
+    }
+}
